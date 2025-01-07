62a500df1665e120ee35fa4871a2b6af7fc12bf1
aayushverma191 <aayushverma4481@gmail.com>
2025-01-07 17:15:33 +0530

    backend.tf

--- MySQLtool/Mysql-Infra/backend.tf
+++ MySQLtool/Mysql-Infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ninja-tool"
+    bucket         = "mysql-final-tool"
     key            = "terraform.tfstate"
     region         = "eu-north-1"
     dynamodb_table = "terraform-lock"
